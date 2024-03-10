fdefccab9df7e76d4422c74cf5b1962eb88aa457
hawerforerouniandes <h.foreror@uniandes.edu.co>
2024-03-10 12:04:37 -0500

    experiment property get

--- src/pda_propiedades_query/api/propiedades_queries.py
+++ src/pda_propiedades_query/api/propiedades_queries.py
@@ -12,12 +12,15 @@ bp = api.crear_blueprint('propiedad', '/propiedad')
 @bp.route('/<id>', methods=('GET',))
 def dar_propiedad(id=None):
     if id:
+        try:
+            id_int = int(id)
+        except ValueError:
+            return make_response(jsonify({"error": "El ID proporcionado no es válido"}), 400)
         
         sp = ServicioPropiedad()
         map_propiedad = MapeadorPropiedadDTOJson()
     
-        return map_propiedad.dto_a_externo(sp.obtener_propiedad_por_id(id))
+        return map_propiedad.dto_a_externo(sp.obtener_propiedad_por_id(id_int))
     
     else:
         return make_response(jsonify({"error": "ID del contrato no proporcionado"}), 400)
-    
